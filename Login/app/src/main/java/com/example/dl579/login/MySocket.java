764cdee356fc727c720bda3cb9940c4e4067a334
LeeSangHoon <dl57934@naver.com>
2017-07-02 16:58:59 +0900

    자동 로그인 완성

---
+++ Login/app/src/main/java/com/example/dl579/login/MySocket.java
@@ -0,0 +1,31 @@
+package com.example.dl579.login;
+
+
+import java.net.Socket;
+
+/**
+ * Created by dl579 on 2017-07-01.
+ */
+
+class MySocket {
+    static  Socket socket = null;
+   static  String IP = "192.168.43.153";
+    private static final MySocket ourInstance = new MySocket();
+
+    static MySocket getInstance() {
+        try {
+            socket = new Socket(IP, 7723);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return ourInstance;
+    }
+
+    private MySocket() {
+
+    }
+
+
+}
